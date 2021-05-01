d6a246761aa5ad3d7e3345324c3cc04ed8fee8a9
Nick <nickjbianco25@gmail.com>
2021-05-01 12:22:27 -0400

    Config Rails API Session Store to work in all environments

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,4 +1,7 @@
-Rails.application.config.session_store :cookie_store, key: "_strength_progress_app"
+if Rails.env == "production"
+    # This is for deployment link ... API Source
+    # Rails.application.config.session_store :cookie_store, key: "_strength_progress_app", domain: ""
+else   
+    Rails.application.config.session_store :cookie_store, key: "_strength_progress_app"
+end 
 
-# This is for deployment link ... API Source 
-# , domain: ""
