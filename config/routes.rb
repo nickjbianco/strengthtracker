049761449c794bd7fa530ecf10db63ce4e14cb20
Nick <nickjbianco25@gmail.com>
2021-05-01 12:17:59 -0400

    Add routes and controllers for Registration and Sessions

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # User can login
+  resources :sessions, only: [:create]
+    # Check to see if User is logged in
+  delete :logout, to: "sessions#logout"
+  # Logs out a User who is logged in
+  get :logged_in, to: "sessions#logged_in"
+  # User can create an account
+  resources :registrations, only: [:create]
+
+  root to: "static#home"
 end
